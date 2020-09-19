a427e94764be98450800eb63c151d309342bf9f9
Jacob Hoss <jacobhenryhoss2@gmail.com>
2020-09-19 13:24:59 -0400

    added async functions that can be called in map.js

--- config/map.js
+++ config/map.js
@@ -1,14 +1,32 @@
-const states = require("./states.js")
+const states = require("../states.json");
+const userZipCode = require("./states.js");
+const getBreweries = require("./breweries.js");
 
+userZipCode("30114").then(function (res) {
+    let stateCode = res.state;
+    let city = res.city;
+    let longitude = res.lng;
+    let latitude = res.lat;
+    let stateName;
+
+    for (const key in states) {
+        if (`${key}` === stateCode) {
+            stateName = `${states[key]}`;
+        }
+    }
+    getBreweries(city, stateName)
+        .then(breweryList => console.log(breweryList)
+        )
+        .catch(err => console.log(err));
+});
 function initMap() {
-  const reformation = { lat: 34.2351928, lng: -84.4953245 };
-  const sweetwater = { lat: 33.80802235, lng: -84.3810721937623};
-
     const map = new google.maps.Map(
-    $('#map'), { zoom: 15, center: reformation });
-
-    const marker = new google.maps.Marker({ position: reformation, map: map });
-    const marker2 = new google.maps.Marker({ position: sweetwater, map: map };
+        document.getElementById('map'), { zoom: 15, center: { lat: latitude, lng: longitude } });
+    for (let i = 0; i < breweries.breweryList.length; i++) {
+        marker = new google.maps.Marker({ position: { lat: breweries.breweryList[i].latitude, lng: breweries.breweryList[i].longitude }, map: map });
+    }
 }
 
-// src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBc4zOKsWDDNrNX567-_LsnKiOLabv9A3A&callback=initMap">
+/* <script defer
+src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBc4zOKsWDDNrNX567-_LsnKiOLabv9A3A&callback=initMap">
+</script> THIS NEEDS TO BE ADDED TO THE HTML FILE THAT HOSTS THE MAP */
--- config/states.js
+++ config/states.js
@@ -1,13 +1,6 @@
-const states = require("../states.json");
 const axios = require("axios");
 
-let city,
-    longitude,
-    latitude,
-    stateCode,
-    stateName;
-
-function userZipCode(zipCode) {
+async function userZipCode(zipCode) {
   const clientKey =
     "Ez9Rro9HCYnywWiEiSljGWM9oO69YSKFWh58p0WjAL1CBhEjIo7FsDGesuor38Ev";
   const url =
@@ -16,17 +9,9 @@ function userZipCode(zipCode) {
     "/info.json/" +
     zipCode +
     "/radians"; // need to pass in user input
-    axios.get(url).then(function (res) {
-        console.log(res.data);
-        stateCode = res.data.state;
-        city = res.data.city;
-        longitude = res.data.lng;
-        latitude = res.data.lat;
-    }).then(function (res) {
-        for (const key in states) {
-            if (`${key}` === stateCode) {
-                stateName = `${states[key]}`;
-            }
-        }
-    });
-}
+    const res = await axios.get(url);
+        return res.data;
+    };
+
+
+module.exports = userZipCode
