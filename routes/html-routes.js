dc3acc9b43c627012e2548890cb0d3572454fd25
Jacob Hoss <jacobhenryhoss2@gmail.com>
2020-09-22 22:39:53 -0400

    made it so memberZipCode is available for initMap

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,5 +1,6 @@
 // Requiring path to so we can use relative routes to our HTML files
 const path = require("path");
+const fs = require("fs");
 
 // Requiring our custom middleware for checking if a user is logged in
 const isAuthenticated = require("../config/middleware/isAuthenticated");
@@ -33,6 +34,24 @@ module.exports = function(app) {
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
+
+    console.log(req.user);
+    const data = fs.readFileSync(
+      path.join(__dirname, "../public/members.html"),
+      "utf8"
+    );
+    
+    //this route can use req.user.zipCode to make an axios call to our zipcode api and get lat/lng
+    //.then(data => {
+      //res.send(data.replace("{{memberLat}}", latFromAPI).replace("memberLng", lngFromAPI));
+
+    //})
+    // .catch(err => {
+      //don't replace the data. If we don't replace, we need to check on the front end
+      //to see if the placeholders still exist and use navigator.geolocation to center the map instead
+    // })
+
+
+    res.send(data.replace("{{memberZip}}", req.user.zipCode));
   });
 };
